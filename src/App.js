7411052ece33619311e57a4e126418e7c3944f04
Tanuj Sengupta <tanuj.sengupta@sap.com>
2022-07-27 07:50:30 +0530

    First commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css'
+import ColorRenderer from './01-color-renderer/ColorRenderer'
+
+function App () {
+  return (
+    <div className='App'>
+      <ColorRenderer />
+    </div>
+  )
+}
+
+export default App
